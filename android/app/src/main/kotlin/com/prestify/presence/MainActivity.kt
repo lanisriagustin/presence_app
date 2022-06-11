47ce5fb827fb5724b8d80a4319af6d06e2785477
Lanisa <lanisriagustin137@gmail.com>
2022-06-11 14:12:54 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/prestify/presence/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.prestify.presence
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/company_data.dart
@@ -0,0 +1,8 @@
+class CompanyData {
+  static String defaultPassword = 'qwertyuiop';
+  static String defaultRole = 'employee';
+  static Map<String, dynamic> office = {
+    'latitude': -7.3236478,
+    'longitude': 108.3265404,
+  };
+}
